7f383a9b74cea169bc88d7393a08955fe7863b38
Abraham Gonzalez <gnz.abr@gmail.com>
2014-07-27 20:15:56 -0500

    Primer Commit, gemfile,workshop index, cliente index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 LotsoBear::Application.routes.draw do
+
+
+  match '/w',  to: 'workshop#index', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
