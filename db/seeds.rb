5f9af503068456005bd9df49ba1becd6e11db728
Abraham Gonzalez <gnz.abr@gmail.com>
2014-08-05 18:10:35 -0500

    default users added to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create!(name:'admin',username:'admin',password:'admin',email:'admin@admin.com', privileges:'admin',password_confirmation:'admin')
+
+operador = User.create!(name:'operador',username:'operador',password:'operador',email:'operador@operador.com', privileges:'operador',password_confirmation:'operador')
+
+capturista = User.create!(name:'capturista',username:'capturista',password:'capturista',email:'capturista@capturista.com', privileges:'capturista',password_confirmation:'capturista')
+
+gerente = User.create!(name:'gerente',username:'gerente',password:'gerente',email:'gerente@gerente.com', privileges:'gerente',password_confirmation:'gerente')
+
+ajustador = User.create!(name:'ajustador',username:'ajustador',password:'ajustador',email:'ajustador@ajustador.com', privileges:'ajustador',password_confirmation:'ajustador')
