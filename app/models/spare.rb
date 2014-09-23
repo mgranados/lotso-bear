be59cae73391a1413c1183e398d54e753bb37f70
Abraham González <gnz.abr@gmail.com>
2014-09-23 12:14:05 -0500

    low cut udpate

--- app/models/spare.rb
+++ app/models/spare.rb
@@ -1,2 +1,4 @@
 class Spare < ActiveRecord::Base
+  has_many :spares
+  accepts_nested_attributes_for :spares, :reject_if => proc { |a| a[:name].blank? || a[:brand].blank? }
 end
--- db/migrate/20140912230748_create_spares.rb
+++ db/migrate/20140912230748_create_spares.rb
@@ -3,6 +3,7 @@ class CreateSpares < ActiveRecord::Migration
     create_table :spares do |t|
       t.integer :car_id
       t.integer :warehouse_id
+      t.integer :spare_id
       t.string :status
       t.text :description
       t.string :code
