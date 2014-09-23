dbdb1c81354df1211e2707a6dfb3b4ddc4c44eab
Abraham González <gnz.abr@gmail.com>
2014-09-23 12:17:45 -0500

    Created Valuation model

---
+++ db/migrate/20140923171729_create_valuations.rb
@@ -0,0 +1,10 @@
+class CreateValuations < ActiveRecord::Migration
+  def change
+    create_table :valuations do |t|
+      t.string :client_code
+      t.string :edited_by
+
+      t.timestamps
+    end
+  end
+end
