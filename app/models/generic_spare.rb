a032e191bd3d34e97e70eae215a88f8c283cd9d1
Abraham González <gnz.abr@gmail.com>
2014-11-05 11:45:37 -0600

    Model Udpated

--- app/models/generic_spare.rb
+++ app/models/generic_spare.rb
@@ -1,6 +1,7 @@
 class GenericSpare < ActiveRecord::Base
   has_many :generic_spares
   has_many :car_spare_alloys
+  has_many :spares
   has_many :generic_cars, through: :car_spare_alloys
 
 end
--- app/models/spare.rb
+++ app/models/spare.rb
@@ -1,4 +1,5 @@
 class Spare < ActiveRecord::Base
   has_many :spares
+  belongs_to :generic_spare
   accepts_nested_attributes_for :spares, :reject_if => proc { |a| a[:name].blank? || a[:brand].blank? }
 end
