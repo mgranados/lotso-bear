531a7390937c0781c3d1df8455ca6a21346a9ba6
Abraham González <gnz.abr@gmail.com>
2014-11-26 17:29:16 -0600

    Changes to generic car, added ability to use vigente

--- app/controllers/generic_cars_controller.rb
+++ app/controllers/generic_cars_controller.rb
@@ -1,110 +1,110 @@
 class GenericCarsController < ApplicationController
   before_action :set_generic_car, only: [:show, :edit, :destroy, :update]
-    def new
-      @generic_car = GenericCar.new
-      # 15.times{  @generic_car.car_spare_alloys.build.build_generic_spare}
-       4.times{  @generic_car.generic_images.build }
-    end
+  def new
+    @generic_car = GenericCar.new
+    # 15.times{  @generic_car.car_spare_alloys.build.build_generic_spare}
+    4.times{  @generic_car.generic_images.build }
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-      @generic_spares = GenericSpare.all
-    end
+  def edit
+    @generic_spares = GenericSpare.all
+  end
 
-    def index
-      @generic_cars = GenericCar.all
-    end
+  def index
+    @generic_cars = GenericCar.all
+  end
 
-    def create
-        @generic_car = GenericCar.new(generic_car_params)
-        @firstyear = generic_car_params[:first_generation_year]
-        @lastyear = generic_car_params[:last_generation_year]
+  def create
+    @generic_car = GenericCar.new(generic_car_params)
+    @firstyear = generic_car_params[:first_generation_year]
+    @lastyear = generic_car_params[:last_generation_year]
 
 
 
-        if @generic_car.save
-          if @lastyear
-            for i in @firstyear...@lastyear
-              @record = Generation.find_by_year(i)
-              @generation =   GenericCarGeneration.new(generic_car_id:@generic_car.id, generation_id: @record.id)
-              @generation.save
-            end
-
-          else
-            @record = Generation.find_by_year(@firstyear)
-            @generation =   GenericCarGeneration.new(generic_car_id:@generic_car.id, generation_id: @record.id)
-            @generation.save
-          end
-
-          flash[:succes]= "Guardado Con exito"
-
-          redirect_to action: 'index'
-        else
-          render new
+    if @generic_car.save
+      if @lastyear.to_s != "Vigente"
+        for i in @firstyear.to_i...@lastyear.to_i+1
+          @record = Generation.find_by_year(i)
+          @generation = GenericCarGeneration.new(generic_car_id:@generic_car.id, generation_id: @record.id)
+          @generation.save
         end
-    end
 
-    def update
-      respond_to do |format|
-        if @generic_car.update(generic_car_params)
-          format.html { redirect_to @generic_car, notice: 'El Carro fue editado con exito' }
-          format.json { render :show, status: :ok, location: @generic_car }
-        else
-          format.html { render :edit }
-          format.json { render json: @generic_car.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
-    def destroy
-      @generic_car.destroy
-      flash[:notice] = "Borrado Exitosamente."
-      redirect_to action: 'index'
-    end
-
-
-
-    def generic_car_params
-      params.require(:generic_car).permit(
-        :brand,
-        :type_of_car,
-        :year,
-        :generation,
-        :model,
-        :doors,
-        :first_generation_year,
-        :last_generation_year,
-        generic_images_attributes: [
-          :generic_id,
-          :photo
-        ]
-
-        # car_spare_alloys_attributes: [
-        #   :generic_car_id,
-        #   :generic_spare_id,
-        #   :relation,
-        #   generic_spare_attributes:[
-        #     :type_of_spare,
-        #     :brand,
-        #     :name,
-        #     :region,
-        #     :description
-        #   ]
-        # ]
-    )
-    end
-
-    def set_generic_car
-      @generic_car = GenericCar.find(params[:id])
-    end
-
-    def search
-      if params[:search]
-      @generic_cars = GenericCar.search(params[:search])
       else
+        @record = Generation.find_by_year(@firstyear)
+        @generation = GenericCarGeneration.new(generic_car_id:@generic_car.id, generation_id: @record.id)
+        @generation.save
+      end
+
+      flash[:success]= "Guardado con exito"
+
+      redirect_to action: 'index'
+    else
+      render new
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @generic_car.update(generic_car_params)
+        format.html { redirect_to @generic_car, notice: 'El Carro fue editado con exito' }
+        format.json { render :show, status: :ok, location: @generic_car }
+      else
+        format.html { render :edit }
+        format.json { render json: @generic_car.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+    @generic_car.destroy
+    flash[:notice] = "Borrado Exitosamente."
+    redirect_to action: 'index'
+  end
+
+
+
+  def generic_car_params
+    params.require(:generic_car).permit(
+    :brand,
+    :type_of_car,
+    :year,
+    :generation,
+    :model,
+    :doors,
+    :first_generation_year,
+    :last_generation_year,
+    generic_images_attributes: [
+      :generic_id,
+      :photo
+    ]
+
+    # car_spare_alloys_attributes: [
+    #   :generic_car_id,
+    #   :generic_spare_id,
+    #   :relation,
+    #   generic_spare_attributes:[
+    #     :type_of_spare,
+    #     :brand,
+    #     :name,
+    #     :region,
+    #     :description
+    #   ]
+    # ]
+    )
+  end
+
+  def set_generic_car
+    @generic_car = GenericCar.find(params[:id])
+  end
+
+  def search
+    if params[:search]
+      @generic_cars = GenericCar.search(params[:search])
+    else
+    end
+  end
 
 end
