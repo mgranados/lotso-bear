cd15d5ce377bc8c11d3b8e884db3561e3542bf3b
Abraham González <gnz.abr@gmail.com>
2015-01-02 21:26:34 -0600

    woot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require jquery.turbolinks
+//= require jquery-ui
 //= require turbolinks
 //= require bootstrap.min
 //= require_tree .
