f5839212a324d8e0dd66da39e6a1617190fe1dda
Martin Granados <margrangr@gmail.com>
2015-02-12 12:58:35 -0600

    multiple images in cars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,10 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require bootstrap.min
-
+//= require jquery-fileupload
 //= require turbolinks
 //= require jquery.turbolinks
-
+//= require jquery-fileupload/basic-plus
 //= require cocoon
 //= require dataTables/jquery.dataTables
 
