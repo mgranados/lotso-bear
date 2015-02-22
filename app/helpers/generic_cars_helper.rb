3ef5b59363b027dd0367840084ae856c7d7a3b1f
Martin Granados <margrangr@gmail.com>
2015-02-22 00:12:12 -0600

    bug initials helper

--- app/helpers/generic_cars_helper.rb
+++ app/helpers/generic_cars_helper.rb
@@ -1,8 +1,11 @@
 module GenericCarsHelper
   def convert_code(str)
-    code = Array.new(str.split('-'))
-    code[0] = Brand.find_by_id(code[0]).acronym
-    code[2] = ModelAcronym.find_by_id(code[2]).initials
-    code.join
+  	unless str.blank?
+	    code = Array.new(str.split('-'))
+	    code[0] = Brand.find_by_id(code[0]).acronym
+	    code[2] = ModelAcronym.find_by_id(code[2]).initials
+	    code = code.join
+	end
+	code
   end
 end
