81755bea7f8bc13719fd78bbddaeac2d3ddb9128
Martin Granados <margrangr@gmail.com>
2015-02-22 12:53:52 -0600

    bug convert code

--- app/helpers/generic_cars_helper.rb
+++ app/helpers/generic_cars_helper.rb
@@ -1,6 +1,6 @@
 module GenericCarsHelper
   def convert_code(str)
-  	if !str.blank?
+  	if str
 	    code = Array.new(str.split('-'))
 	    code[0] = Brand.find_by_id(code[0]).acronym
 	    code[2] = ModelAcronym.find_by_id(code[2]).initials
