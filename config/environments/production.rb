587d59bd853f8ba66d45bd47197036dd3eb2dbbd
Martin Granados <margrangr@gmail.com>
2015-02-26 22:25:45 -0600

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ LotsoBear::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.compile = true
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
