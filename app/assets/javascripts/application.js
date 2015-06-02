1857a3992b0a04e047d2468b93439bbd285871b4
Martin Granados <margrangr@gmail.com>
2015-06-02 13:03:37 -0500

    Header UI changes now it is working in mobile. Also focus w javascript  on departures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require bootstrap
 //= require dataTables/jquery.dataTables
 //= require dataTables/extras/dataTables.responsive
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require bootstrap.min
 //= require turbolinks
 //= require jquery.turbolinks
 //= require dataTables/jquery.dataTables
