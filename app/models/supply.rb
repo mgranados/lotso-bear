3603bc5ad64c5549bb1a9200fb82c3fbd3de2f69
Francisco Paniagua Reyes <FranciscoPR@MacBook-Pro-de-Francisco.local>
2015-07-20 18:42:47 -0500

    validar supplies

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,4 +1,8 @@
 class Supply < ActiveRecord::Base
 	belongs_to :supplier
 	has_many :spendings
+
+	validates :name, :minimum_qty, :cost, :description, :quantity,  :code, presence: true
+  	validates :code, uniqueness: true
+
 end
