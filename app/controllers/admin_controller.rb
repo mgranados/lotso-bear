5fbd90c5073c4ea9ca79bbced936cf8adaad5c33
Martin Granados <martinn@The-Macbook-Air.local>
2015-08-11 22:46:12 -0500

    antes de la tormenta

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,10 +3,13 @@ class AdminController < ApplicationController
   def home
   end
 
+
+
   def prices
     @families_priced = GenericFamily.joins(:supplier_codes).where.not(supplier_codes: {price_centavos: nil}).where.not(father_id: nil)
     @families_unpriced = GenericFamily.joins(:supplier_codes).where.not(father_id: nil).where(supplier_codes: {price_centavos: nil})
   end
+
 
   def suppliers
       @suppliers = Supplier.all
